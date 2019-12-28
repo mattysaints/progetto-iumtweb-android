227239ea6f6f2131ac093b89c3414fc2ab923ef0
mattia.marra <mattia.marra@edu.unito.it>
2019-12-28 20:04:47 +0100

    Creazione Dashboard e vari fragment

---
+++ app/src/main/java/com/ium/unito/progetto_ium_tweb1/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.ium.unito.progetto_ium_tweb1.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is gallery fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
