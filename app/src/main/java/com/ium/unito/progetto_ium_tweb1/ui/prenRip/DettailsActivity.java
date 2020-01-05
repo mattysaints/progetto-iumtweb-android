2aba38a66941f2e300f6669f9a00fb52f1e04179
mattia.marra <mattia.marra@edu.unito.it>
2020-01-05 01:12:56 +0100

    Aggiunta Dettails View per la prenotazione della ripetizione

---
+++ app/src/main/java/com/ium/unito/progetto_ium_tweb1/ui/prenRip/DettailsActivity.java
@@ -0,0 +1,33 @@
+package com.ium.unito.progetto_ium_tweb1.ui.prenRip;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.ium.unito.progetto_ium_tweb1.R;
+
+public class DettailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dettails);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
