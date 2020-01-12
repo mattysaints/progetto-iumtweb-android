50072eae98d4406c5345ca925850710ea19b9212
mattia.marra <mattia.marra@edu.unito.it>
2020-01-12 19:29:07 +0100

    Modifica DialogView e correzzione errori , manca solo funzione filtro

--- app/src/main/java/com/ium/unito/progetto_ium_tweb1/ui/prenRip/PrenRipFragment.java
+++ app/src/main/java/com/ium/unito/progetto_ium_tweb1/ui/prenRip/PrenRipFragment.java
@@ -2,6 +2,7 @@ package com.ium.unito.progetto_ium_tweb1.ui.prenRip;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,6 +11,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -83,10 +88,58 @@ public class PrenRipFragment extends Fragment {
         switch (id) {
             case R.id.filtra:
                 builder = new AlertDialog.Builder(getContext());
+                LayoutInflater inflater = this.getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.dialog_layout,null);
+                builder.setView(dialogView);
+
+                final EditText Docente = dialogView.findViewById(R.id.DocPText);
+                final EditText Corso = dialogView.findViewById(R.id.CorPtext);
+                final Spinner Ora = dialogView.findViewById(R.id.spinner2);
+                final Spinner Giorno = dialogView.findViewById(R.id.spinner3);
+
+                String[] ore = new String[] {
+                        "15-16", "16-17", "17-18", "18-19"
+                };
+                String[] giorni = new String[] {
+                        "Lun", "Mar", "Mer", "Gio" , "Ven"
+                };
+
+                Spinner s2 = (Spinner) dialogView.findViewById(R.id.spinner2);
+                ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getContext(),
+                        android.R.layout.simple_spinner_item, ore);
+                adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                s2.setAdapter(adapter2);
+
+                Spinner s3 = (Spinner) dialogView.findViewById(R.id.spinner3);
+                ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(getContext(),
+                        android.R.layout.simple_spinner_item, giorni);
+                adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                s3.setAdapter(adapter3);
+
                 builder.setMessage("Filtra Ripetizioni");
+
+                builder.setPositiveButton("Cerca", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        filtraRipetizioni(Docente.getText().toString(),Corso.getText().toString(),Ora.getSelectedItem().toString(),Giorno.getSelectedItem().toString());
+                        dialogInterface.dismiss();
+                    }
+                });
+                builder.setNegativeButton("Cancella", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                alertDialog = builder.create();
+                alertDialog.show();
                 break;
         }
 
         return false;
+    }
+
+    private void filtraRipetizioni(String docente, String corso, String ora, String giorno) {
+        //CODICE FILTRAGGIO PRENOTAZIONI
     }
 }
